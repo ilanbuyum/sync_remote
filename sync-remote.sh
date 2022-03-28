86267f6a74ec48833875ddecbf33f55e31f80a39
ilanbuyum <ilanbm@gmail.com>
2022-03-28 14:20:10 +0300

    Create sync-remote.sh

---
+++ sync-remote.sh
@@ -0,0 +1,33 @@
+#
+# Should be run from project root dir (where .git is)
+#
+
+srcFile=$1
+
+if [ -z ${REMOTE_PATH+x} ]; then
+  echo "REMOTE_PATH not set. Please export REMOTE_PATH=user@host:/path/to/remote/root"
+  exit 1
+fi
+
+if [ "$srcFile" == "" ]; then
+  cat <<EOF
+  
+Copy file from a local project root to a remote project root exported by REMOTE_PATH.
+
+Usage: sync-remote <SourceFile>
+
+The command should be run from the root of the local project.
+SourceFile should include the full relative path to its project root. 
+
+If the following is set:
+SourceFile=/home/build/proj1/module1/src/foo.c 
+REMOTE_PATH=user@host:/home/build/project 
+then running: sync-remote module1/src/foo.c
+from: /home/build/proj1
+will result with: scp module1/src/foo.c user@host:/home/build/project/module1/src/foo.c
+Remote dir is assumed to exist.
+EOF
+  exit 1
+fi
+
+scp $srcFile $REMOTE_PATH/$srcFile
